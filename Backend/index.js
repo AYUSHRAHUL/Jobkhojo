bb6adf5c9f138705aded38b5f8aea724730f0007
AYUSHRAHUL <142750969+AYUSHRAHUL@users.noreply.github.com>
2025-07-12 16:38:53 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://jobkhojo-nu.vercel.app"],
+  origin: "https://jobkhojo-nu.vercel.app",
   credentials: true,
 }; 
 
